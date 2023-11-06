0346f0d24fb16a922522791b0a43326b729af964
dibi-kamdem <kamdem.dibiye@gmail.com>
2023-11-06 12:59:29 +0100

    first commit

---
+++ Tailwind-css/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: { 
+        regular: '14px',
+        regular1: '20px',
+        regular2: '50px',
+        gras: '16px',
+        gras1: '24px',
+        gras2: '56px',
+        gras3: '72px'
+
+      },
+      colors: { 
+        head: '#5EAE53',
+        btn1: '#EB8E78',
+        btn2: '#5EAE53',
+        hover: '#F49B3F',
+        card: '#F7F7F7',
+        man: '#FFE6E0',
+        woman: '#FFE9CF'
+      },
+      fontFamily: { 
+        dmsans: 'DM Sans',
+        worksans: 'Work Sans',
+        caveat: 'Caveat'
+      }
+    },
+  },
+  plugins: [],
+}
+
